e48228dace67daba9ba9d323e161c3cac0e07f4d
Pantela Vasiliy <pantela1995@gmail.com>
2023-04-25 19:03:40 +0300

    Api fix

--- frontend/composables/useApiFetch.ts
+++ frontend/composables/useApiFetch.ts
@@ -1,8 +1,9 @@
 const dev = import.meta.env.DEV
 const ip = dev ? '127.0.0.1' : '80.78.245.244'
+const port = dev ? '8000' : '80'
 // @ts-ignore
 export const useApiFetch: typeof useFetch = async (path, options?) => {
-  const url = `http://${ip}:8000/api`
+  const url = `http://${ip}:${port}/api`
   await nextTick()
   return useFetch(path, { baseURL: url, ...options })
 }
